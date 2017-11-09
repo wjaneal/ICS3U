0e99e7f8ede0dd1575fa54f07eb15558a2bfeb75
Charlotte418 <31779684+Charlotte418@users.noreply.github.com>
2017-11-09 13:01:54 -0500

    Added FinalProject.py

---
+++ SaltFish/Charlotte/FinalProject.py
@@ -0,0 +1,9 @@
+#Individual Computer Programming Plan
+#Name: Charlotte Chen
+#Topic: Scanning Tunnelling Microscope
+#Functions of the Complete Program:
+  #Displays the voltage with different colours(e.g. Voltage 0-white,Voltage 1-black
+  #Displays the current coordinates of points that are being scanned
+  #Displays a graph according to the simulation program(input program)
+  #Connects to Arduino
+
