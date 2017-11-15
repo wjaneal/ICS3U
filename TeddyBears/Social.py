77adb0b468a338b454911d79febe0b7bf30145e1
Kia <33462862+Kiawrash@users.noreply.github.com>
2017-11-15 14:08:03 -0500

    Add Social

---
+++ TeddyBears/Social.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 13:44:22 2017
+
+@author: callm
+"""
+
+import pygame
+import sys
+from tkinter import *
+
+pygame.init()
+
+def value_Cs():
+    num1.set("C#")
+    sound = pygame.mixer.Sound( "C:\User\callm\Desktop\G")
+    sound.play()
+    return
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+root.title('PIANO')
+
+num1=StringVar()
+
+
