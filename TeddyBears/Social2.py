885a2df4fa24ff4cf3d812e0a245ffd560a659d5
Kia <33462862+Kiawrash@users.noreply.github.com>
2017-11-20 13:53:48 -0500

    Social number 2 added

---
+++ TeddyBears/Social2.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 13:20:43 2017
+
+@author: callm
+"""
+
+import pygame
+import sys
+from tkinter import *
+
+pygame.init()
+
+def value_Cs():
+    num1.set("C#")
+    sound = pygame.mixer.Sound("C:\\Users\\fy\\Music\\aiyucheng.mp3")
+    sound.play()
+    return
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+root.title('BIANO')
+
+num1=StringVar()
+
+topframe = Frame(root)
+topframe.pack(side = TOP)
+txtDisplay=Entry(frame, textvariable = num1, bd=20, insertwidth =1, font=30, justify="center", width=4,)
+txtDisplay.pack( side = TOP)
+
+button1 = Button(topframe,padx=8, height = 6, pady=8, bd=8, text="C# ", bg="black",fg="white",command=value_Cs)
+button1.pack(side =LEFT)
+button22 = Button(topframe, state=DISABLED, height = 7, width=1, padx=0, pady=0, relief=RIDGE)
+button22.pack(side =LEFT)
+button2 = Button(topframe,padx=8, height = 6, pady=8, bd=8, text="D# ", bg="black",fg="white",command=value_Ds)
+button2.pack(side =LEFT)
+button22 = Button(topframe, state=DISABLED, height = 7, width=1, padx=0, pady=0, relief=RIDGE)
+button22.pack(side =LEFT)
+button3 = Button(topframe,padx=8, height = 6, pady=8, bd=8, text="F# ", bg="black",fg="white",command=value_Fs)
+button3.pack(side =LEFT)
+button22 = Button(topframe, state=DISABLED, height = 7, width=1, padx=0, pady=0, relief=RIDGE)
+button22.pack(side =LEFT)
+button4 = Button(topframe,padx=8, height = 6, pady=8, bd=8, text="G# ", bg="black",fg="white",command=value_Gs)
+button4.pack(side =LEFT)
+button22 = Button(topframe, state=DISABLED, height = 7, width=1, padx=0, pady=0, relief=RIDGE)
+button22.pack(side =LEFT)
+
+button2 = Button(topframe,padx=8, pady=8, height = 6, bd=8, text"Bb ", bg="black",fg="white",command=value_Bb)
+button2.pack(side =LEFT)
+button22 = Button(topframe, state=DISABLED, height = 7, width=4, padx=0, pady=0, relief=RIDGE)
+button22.pack(side =LEFT)
+button3 = Button(topframe,padx=8, pady=8, height = 6, bd=8, text"C#1 ", bg="black",fg="white",command=value_Cs1)
+button3.pack(side =LEFT)
+button22 = Button(topframe, state=DISABLED, height = 7, width=4, padx=0, pady=0, relief=RIDGE)
+button22.pack(side =LEFT)
+button4 = Button(topframe,padx=8, pady=8, height = 6, bd=8, text"D#1 ", bg="black",fg="white",command=value_Ds1)
+button4.pack(side =LEFT)
