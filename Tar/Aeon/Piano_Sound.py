e2224ddc1808b60e32b1c89abd5bf7f9b74d7c6a
xly2000416 <31554408+xly2000416@users.noreply.github.com>
2017-11-29 14:11:25 -0500

    Reega Updated Files

--- Tar/Aeon/Piano_Sound.py
+++ Tar/Aeon/Piano_Sound.py
@@ -20,94 +20,117 @@ def value_C4():
 def value_C4s():
     num1.set("C4s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-C4#.mp3")
+    sound.play()
     return                           
 def value_D4():
     num1.set("D4")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-D4.mp3")
+    sound.play()
     return                           
 def value_D4s():
     num1.set("D4s")
-    sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-D4#.mp3") 
+    sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-D4#.mp3")
+    sound.play()
     return                           
 def value_E4():
     num1.set("E4")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-E4.mp3")
+    sound.play()
     return                           
 def value_F4():
     num1.set("F4")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-F4.mp3")
+    sound.play()
     return
 def value_F4s():
     num1.set("F4s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-F4#.mp3")
+    sound.play()
     return
 def value_G4():
     num1.set("G4")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-G4.mp3")
+    sound.play()
     return
 def value_G4s():
     num1.set("G4s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-G4#.mp3")
+    sound.play()
     return
 def value_A4():
     num1.set("A4")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-A4.mp3")
+    sound.play()
     return
 def value_A4s():
     num1.set("A4s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-A4#.mp3")
+    sound.play()
     return
 def value_B4():
     num1.set("B4")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-B4.mp3")
+    sound.play()
     return
 def value_C5():
     num1.set("C5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-C5.mp3")
+    sound.play()
     return
 def value_C5s():
     num1.set("C5s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-C5#.mp3")
+    sound.play()
     return
 def value_D5():
     num1.set("D5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-D5.mp3")
+    sound.play()
     return
 def value_D5s():
     num1.set("D5s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-D5#.mp3")
+    sound.play()
     return
 def value_E5():
     num1.set("E5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-E5.mp3")
+    sound.play()
     return
 def value_F5():
     num1.set("F5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-F5.mp3")
+    sound.play()
     return
 def value_F5s():
     num1.set("F5s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-F5#.mp3")
+    sound.play()
     return
 def value_G5():
     num1.set("G5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-G5.mp3")
+    sound.play()
     return
 def value_G5s():
     num1.set("G5s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-G5#.mp3")
+    sound.play()
     return
 def value_A5():
     num1.set("A5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-A5.mp3")
+    sound.play()
     return
 def value_A5s():
     num1.set("A5s")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-A5#.mp3")
+    sound.play()
     return
 def value_B5():
     num1.set("B5")
     sound = pygame.mixer.Sound("Desktop/Piano_Keys_mp3/piano_keys_soundMP3/40-B5.mp3")
+    sound.play()
     return
 
 
