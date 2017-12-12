754f1ad2df9846cc52b9842b15644086a9a00233
Charlotte418 <31779684+Charlotte418@users.noreply.github.com>
2017-12-12 18:13:28 -0500

    added programs for Arduino

--- SaltFish/Group Project/banker's case.py
+++ SaltFish/Group Project/banker's case.py
@@ -12,36 +12,48 @@ from tkinter import font
 import math
 from random import shuffle
 
-flag=0
+flag=0 #initializes flag as 0
 
 class MenuBar(Menu):
     def destroywin(self,filewinc):
-        filewinc.destroy()
-#instruction introductiob
+        '''
+        Parameters:
+            self - MenuBar
+            filewinc - a window you want to close
+        Return - None
+        '''
+        filewinc.destroy() #closes a window
     def start2(self):
-        self.filewin3.destroy()
-        self.filewin2 = Toplevel(self)
-        self.instructions = Label(self.filewin2,text="Please select three cases you want to open.",bg="lightgreen").grid(row=0,column=0)
-        self.okbutton = Button(self.filewin2,text="OK",bg="lightgreen",command=lambda:self.destroywin(self.filewin2)).grid(row=1,column=0)
-        helv18b = font.Font(family="Helvetica",size=18,weight="bold")
-        helv16 = font.Font(family="Helvetica",size=16,weight="bold")
-        filewin = Toplevel(self)
+        '''
+        Parameters:
+            self - MenuBar
+        Return - None
+        '''
+        self.filewin3.destroy() #closes the previous window
+        self.filewin2 = Toplevel(self) #opens a new window
+        self.instructions = Label(self.filewin2,text="Please select three cases you want to open.",bg="lightgreen").grid(row=0,column=0) #displays instructions
+        self.okbutton = Button(self.filewin2,text="OK",bg="lightgreen",command=lambda:self.destroywin(self.filewin2)).grid(row=1,column=0) 
+        #creates a button which makes instructions disappear when clicking "OK"
+        helv18b = font.Font(family="Helvetica",size=18,weight="bold") #defines fonts
+        helv16 = font.Font(family="Helvetica",size=16,weight="bold") #defines fonts
+        filewin = Toplevel(self) #opens a new window
         self.Cases=[1,2,5,10,100,250,500,750,1000,2500,5000,10000,25000,50000,75000,100000,200000,250000,500000,750000,1000000,2000000,2500000,5000000,7500000,10000000]
-        self.money=[]
-        self.offer=0
-        self.sum=0
-        shuffle(self.Cases)
-        for i in range(0,len(self.Cases)):
+        #creates a list containing 26 numbers as the amount of money for 26 cases
+        self.money=[] #creates an empty list
+        #self.offer=0 #initializes self.offer as 0
+        #self.sum=0 #initializes self.sum as 0
+        shuffle(self.Cases) #ramdomly changes the order of the list
+        for i in range(0,len(self.Cases)): #uses a loop to append elements in self.Cases to self.money
             self.money.append(self.Cases[i])
-        self.Label26=[]
+        self.Label26=[] #creates a list for 26 labels
         for i in range(0,26):
-            self.Label26.append(Label(filewin,text=self.Cases[25-i]))
-            self.Label26[i].grid(row=7+int(i/5),column=(i%5))
-        #self.Label1 = Label(filewin,text=self.Cases[0])
-        #self.Label1.grid(row=6,column=0)
+            self.Label26.append(Label(filewin,text=self.Cases[25-i])) #creates 26 labels by using a loop
+            self.Label26[i].grid(row=7+int(i/5),column=(i%5)) #places the labels
         #create buttons for the cases
         self.Button1 = Button(filewin,text="Case 1",bg="lightgreen",command=lambda:self.open_case(self.Button1,self.money[0],self.Label26[25]))
+        #When clicking the button, sends the current button, the money in the case, and the label displaying the money to the function self.open_case
         self.Button1.grid(row=0,column=0)
+        #places the button
         self.Button2 = Button(filewin,text="Case 2",bg="lightgreen",command=lambda:self.open_case(self.Button2,self.money[1],self.Label26[24]))
         self.Button2.grid(row=0,column=1)
         self.Button3 = Button(filewin,text="Case 3",bg="lightgreen",command=lambda:self.open_case(self.Button3,self.money[2],self.Label26[23]))
@@ -93,106 +105,144 @@ class MenuBar(Menu):
         self.Button26 = Button(filewin,text="Case 26",bg="lightgreen",command=lambda:self.open_case(self.Button26,self.money[25],self.Label26[0]))
         self.Button26.grid(row=5,column=0)
         self.remoneylabel = Label(filewin,text="Remaining money",bg="lightgreen").grid(row=6,column=0,columnspan=2)
-            
+        #creates a label which shows the words "Remaining money"
     
     def offers(self):
-        self.filewin1 = Toplevel(self)
-        self.offer=0
-        self.sum=0
-        for i in range(0,len(self.Cases)):
-            self.money.append(self.Cases[i])
-            self.sum+=self.Cases[i]
-        self.offer=self.sum/len(self.Cases)
-        self.offer1.set(int(self.offer))
-        #display the bank's offer
-        self.label1=Label(self.filewin1,text="Hi,this is the bank",bg="lightgreen").grid(row=0,column=0)
+        '''
+        Parameters:
+            self - MenuBar
+        Return - None
+        '''
+        self.filewin1 = Toplevel(self) #opens a new window
+        self.offer=0 #initializes self.offer as 0
+        self.sum=0  #initializes self.sum as 0
+        for i in range(0,len(self.Cases)): #uses a loop to iterate through the list
+            self.sum+=self.Cases[i] #calculate the sum of the money in the remaining cases
+        self.offer=self.sum/len(self.Cases) #calculates the offer from the bank by taking the average of the money in remaining cases 
+        self.label1=Label(self.filewin1,text="Hi,this is the bank",bg="lightgreen").grid(row=0,column=0) #displays greetings from the bank
         self.label2=Label(self.filewin1,text="We would like to offer you an offer of $",bg="lightgreen").grid(row=1,column=0)
         self.offerlabel=Label(self.filewin1,textvariable=self.offer1).grid(row=1,column=1)
-        self.label3=Label(self.filewin1,text="Would you like to accept the offer?",bg="lightgreen").grid(row=2,column=0)
+        self.offer1.set(int(self.offer)) #displays the bank's offer in the dialog box
+        self.label3=Label(self.filewin1,text="Would you like to accept the offer?",bg="lightgreen").grid(row=2,column=0) #asks the user whether he wants to accept the offer or not
         self.yesbutton=Button(self.filewin1,text="Yes",command=self.end)
-        self.yesbutton.grid(row=3,column=0)
+        #creates a button for accepting the offer
+        self.yesbutton.grid(row=3,column=0) #places the "yes" button
         self.nobutton=Button(self.filewin1,text="No",command=self.no)
-        self.nobutton.grid(row=3,column=1)
+        #creates a button for not accepting the offer
+        self.nobutton.grid(row=3,column=1) #places the "no" button
     
     def open_case(self,cButton,n,cLabel):
-        global flag
-        cButton.grid_remove()
-        #cLabel.grid_remove()
-        cLabel.grid_remove()
-        self.Cases.remove(n)
-        if(len(self.Cases)==1 and flag==0):
-            self.end()
-        elif((26-len(self.Cases))%3==0 and len(self.Cases)!=26 and len(self.Cases)>=2 and flag==0):
-            self.offers()
-        elif(len(self.Cases)==1 and flag==1):
-            self.end()
+        '''
+        Parameters:
+            self - MenuBar
+            cButton - the button that represents the case being selected
+            n - the amount of money in the case being selected
+            cLabel - the label that displays the amount of money in the case being selected
+        Return - None
+        '''
+        global flag #declares a global variable flag
+        cButton.grid_remove() #makes the button disappear
+        cLabel.grid_remove() #makes the label disappear
+        self.Cases.remove(n) #removes the amount of money being selected from the list
+        if(len(self.Cases)==1 and flag==0): #If there is only one case left...
+            self.end() #ends the game
+        elif((26-len(self.Cases))%3==0 and len(self.Cases)!=26 and len(self.Cases)>=2 and flag==0): #Every time the user selects 3 cases and there are more than one case left...
+            self.offers() #displays the bank's offer
+        elif(len(self.Cases)==1 and flag==1): #If there is only one case left...
+            self.end() #ends the game
         
     
     def no(self):
-        if (len(self.Cases)>2):
-            self.filewin1.destroy()
-            self.filewin2=Toplevel(self)
-            self.instructions = Label(self.filewin2,text="Please select three cases you want to open.",bg="lightgreen").grid(row=0,column=0)
-            self.okbutton = Button(self.filewin2,text="OK",bg="lightgreen",command=lambda:self.destroywin(self.filewin2)).grid(row=1,column=0)
-        elif(len(self.Cases)<=2):
-            self.filewin1.destroy()
-            self.filewin5=Toplevel(self)
-            self.instructions = Label(self.filewin5,text="Please open one of the cases.",bg="lightgreen").grid(row=0,column=0)
+        '''
+        Parameters:
+            self - MenuBar
+        Return - None
+        '''
+        if (len(self.Cases)>2): #If there are more than 2 cases left...
+            self.filewin1.destroy() #closes the dialog box for deciding whether or not to accept the offer
+            self.filewin2=Toplevel(self) #opens a new window
+            self.instructions = Label(self.filewin2,text="Please select three cases you want to open.",bg="lightgreen").grid(row=0,column=0) #displays instructions
+            self.okbutton = Button(self.filewin2,text="OK",bg="lightgreen",command=lambda:self.destroywin(self.filewin2)).grid(row=1,column=0) #creates an "OK" button;When clicking it, closes the instruction window
+        elif(len(self.Cases)<=2): #If there are less than 3 cases left...
+            self.filewin1.destroy() #closes the dialog box for deciding whether or not to accept the offer
+            self.filewin5=Toplevel(self) #opens a new window
+            self.instructions = Label(self.filewin5,text="Please open one of the cases.",bg="lightgreen").grid(row=0,column=0) #displays instructions
             self.okbutton = Button(self.filewin5,text="OK",bg="lightgreen",command=lambda:self.destroywin(self.filewin5)).grid(row=1,column=0)
     
     def end(self):
-        global flag
-        self.filewin1.destroy()
-        if (len(self.Cases)==1 and flag==0):
-            self.filewin6=Toplevel(self)
+        global flag #declares a global variable flag
+        self.filewin1.destroy() #closes the window
+        if (len(self.Cases)==1 and flag==0): #If there is only one case left...
+            self.filewin6=Toplevel(self) #opens a new window
             self.result=Label(self.filewin6,text="You get $",bg="lightgreen").grid(row=0,column=0)
             self.result2=Label(self.filewin6,text=self.Cases[0],bg="lightgreen").grid(row=0,column=1)
-            if (self.offer<=self.Cases[0]):
+            #displays the money the user gets
+            if (self.offer<=self.Cases[0]): #If the offer is less than or equal to the money the user gets...
                 self.result3=Label(self.filewin6,text="Wise Choice!",bg="lightgreen").grid(row=1,column=0)
+                #displays "Wise Choice!"
             else:
                 self.result3=Label(self.filewin6,text="Bad Luck!",bg="lightgreen").grid(row=1,column=0)
-        elif(len(self.Cases)!=1 and flag==0):
-            self.filewin6=Toplevel(self)
+                #else:displays "Bad Luck!"
+        elif(len(self.Cases)!=1 and flag==0): #If there are more than one case left...
+            self.filewin6=Toplevel(self) #opens a new window
             flag=1
             self.result5=Label(self.filewin6,text="You get $",bg="lightgreen").grid(row=0,column=0)
-            self.result6=Label(self.filewin6,textvariable=self.offer1,bg="lightgreen").grid(row=0,column=1)
+            self.result6=Label(self.filewin6,textvariable=self.offer1,bg="lightgreen").grid(row=0,column=1) #displays the offer
             self.instructions3=Label(self.filewin6,text="Now you can open the remaining cases to see whether you make a good choice.",bg="lightgreen").grid(row=1,column=0)
+            #displays the instruction to let the user open the remaining cases
             self.okbutton2=Button(self.filewin6,text="OK",bg="lightgreen",command=lambda:self.destroywin(self.filewin6)).grid(row=2,column=0)
-        elif(len(self.Cases)==1 and flag==1):
-            self.filewin6=Toplevel(self)
-            if (self.offer>=self.Cases[0]):
+        elif(len(self.Cases)==1 and flag==1): #If there is only one case left...
+            self.filewin6=Toplevel(self) #opens a new window
+            if (self.offer>=self.Cases[0]): #If the offer is more than or equal to the money in the remaining case
                 self.result5=Label(self.filewin6,text="It seems that you have made a wise choice",bg="lightgreen").grid(row=0,column=0)
+                #congratulates the user
             else:
                 self.instructions3=Label(self.filewin6,text="It seems that you are not lucky enough",bg="lightgreen").grid(row=0,column=0)
                
     def donothing(self):
-        pass
+        '''
+        Parameters:
+            self - MenuBar
+        Return - None
+        '''
+        pass #does nothing
     
     def __init__(self, parent):
         Menu.__init__(self, parent)
-        self.offer1 = StringVar()
-        filemenu = Menu(self, tearoff=0) 
-        filemenu.add_command(label="Start", command=self.start) 
-        filemenu.add_command(label="Exit", command=self.quit)
-        self.add_cascade(label="File",menu=filemenu)
-        self.canvas = Canvas(parent,width=1000,height=1000,bg="white")
-        self.filename=PhotoImage(master=self.canvas, file="deal.gif")
+        self.offer1 = StringVar() #declares the Tkinter variable self.offer1
+        filemenu = Menu(self, tearoff=0) #creates a filemenu
+        filemenu.add_command(label="Start", command=self.start) #adds a "Start" command to the filemenu
+        filemenu.add_command(label="Exit", command=self.quit) #adds a "Exit" command to the filemenu
+        self.add_cascade(label="File",menu=filemenu) #names the filemenu "File"
+        self.canvas = Canvas(parent,width=1000,height=1000,bg="white") #creates a rectangular area intended for drawing pictures
+        self.filename=PhotoImage(master=self.canvas, file="deal.gif") #inserts a photo
         self.image1=self.canvas.create_image(500,300,image=self.filename)
         self.canvas.pack()
 
        
     
     def start(self):
-        self.filewin3=Toplevel(self)
-        self.welcome=Label(self.filewin3,text="Welcome to 'The Banker's Case'!",bg="lightgreen").grid(row=0,column=0)
+        '''
+        Parameters:
+            self - MenuBar
+        Return - None
+        '''
+        self.filewin3=Toplevel(self) #opens a new window
+        self.welcome=Label(self.filewin3,text="Welcome to 'The Banker's Case'!",bg="lightgreen").grid(row=0,column=0) #welcomes the user
         self.namelabel=Label(self.filewin3,text="What's your name?",bg="lightgreen").grid(row=1,column=0)
-        self.name=Entry(self.filewin3,bd=5,textvariable=StringVar(value="0")).grid(row=1,column=1)
-        self.sbutton=Button(self.filewin3,text="Start",command=self.start2).grid(row=2,column=0)
+        self.name=Entry(self.filewin3,bd=5,textvariable=StringVar(value="0")).grid(row=1,column=1) #lets the user input his name
+        self.sbutton=Button(self.filewin3,text="Start",command=self.start2).grid(row=2,column=0) #starts the game
         
         
 
     def quit(self):
+        '''
+        Parameters:
+            self - MenuBar
+        Return - None
+        '''
         app.destroy()
+        #quits the program
           
 #Main class - calls menubar class
 class App(Tk):
--- WN/array_ops.py
+++ WN/array_ops.py
@@ -5,20 +5,39 @@
 def Largest(List1):
     largest = List1[0]
     for i in range(1,len(List1)):
-        if largest < List1[i-1]:
-            largest = List1[i-1]
-    return Largest
+        if largest < List1[i]:
+            largest = List1[i]
+    #print(largest)
+    return largest
 n = 3 #How many groups to sort into
 List1 = [5,7,2,78,23,67,45,89,123,6,8,123,454,565,7665,6676,322,4443] 
-Groups = []*n
+Groups = []*n  
+'''Group1=[]
+Group2=[]
+Group3=[]'''
+#print(Groups)
 not_done = True
+i=0
 #repeat until List1 is empty:
 while not_done==True:
-    for i in range(0,len(Groups)):
-        groups[i] = largest(List1)
-    for i in range(0,len(Groups)):
-        groups[n+1-i]= largest(List1)
-print(groups)
+    if (i%2==0 and len(List1)!=0):
+        for i in range(0,n):
+            x=Largest(List1)
+            Groups.append(x)
+            List1.remove(x)
+        i+=1
+        #print(i)
+    elif(i%2==1 and len(List1)!=0):
+        for i in range(0,n):
+            
+            x=Largest(List1)
+            Groups[3-n+].append(x)
+            List1.remove(x)
+        i+=1
+        #print(i)
+    else:
+        not_done = False
+print(Group1,Group2,Group3)
 
     #for each group, 1,2,3...n distribute the largest item in the list
     #for each group, n,n-1,n-2...1 distribute the largest item in the list
