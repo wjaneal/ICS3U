4c5d1c87f74126363d8ce19cd2d41998a021be1a
wjaneal <william.j.a.neal@gmail.com>
2017-12-12 13:08:32 -0500

    Update

--- WN/pygame_sound.py
+++ WN/pygame_sound.py
@@ -4,5 +4,19 @@ pygame.mixer.pre_init(frequency=44100)
 pygame.mixer.init()
 pygame.mixer.music.load('SSF.wav')
 
-pygame.mixer.music.play(0)
+screen = pygame.display.set_mode((400, 300))
+done = False
+try:
+    sound = pygame.mixer.music.play(1)
+    sound.play(loops = -1)
+except:
+    print ("Cannot play sound")
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+                                                                
+    pygame.display.flip()
+
 
