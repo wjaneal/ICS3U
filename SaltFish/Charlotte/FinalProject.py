cd018474ae32c98392051017da7f597d2579ce3b
Charlotte418 <31779684+Charlotte418@users.noreply.github.com>
2017-12-12 21:07:27 -0500

    modified

--- SaltFish/Charlotte/FinalProject.py
+++ SaltFish/Charlotte/FinalProject.py
@@ -2,9 +2,10 @@
 #Name: Charlotte Chen
 #Topic: Scanning Tunnelling Microscope
 #Functions of the Complete Program:
-  #Displays the voltage with different colours(e.g. Voltage 0-white,Voltage 1-black
-  #Displays the current coordinates of points that are being scanned
-  #Displays a graph according to the simulation program(input program)
   #Connects to Arduino
+  #Allows two Arduinos to communicate with each other(one sender and one reader)
+  #Puts voltages into different pins according to the message sent
+  #A graphic user interface which displays the scan dimensions, the current x- and y-coordinates and voltages
+  #A graphic user interface which shows whether the status is on or off by changing the color of the button representing the status
 
 
