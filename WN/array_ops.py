8c8a411c9c6310fab29bea2a1f25ff7819baab43
wjaneal <william.j.a.neal@gmail.com>
2017-12-16 17:36:20 -0500

    Update

--- WN/array_ops.py
+++ WN/array_ops.py
@@ -10,14 +10,22 @@ def Largest(List1):
     return Largest
 n = 3 #How many groups to sort into
 List1 = [5,7,2,78,23,67,45,89,123,6,8,123,454,565,7665,6676,322,4443] 
+#Sorted groups:
+#Groups = [[7665,322,123],[6676,454,],[4443,565]]
 Groups = []*n
 not_done = True
 #repeat until List1 is empty:
 while not_done==True:
     for i in range(0,len(Groups)):
         groups[i] = largest(List1)
+        #Remove it from the list
+    #If len(List1) == 0:
+        #not_done = False
     for i in range(0,len(Groups)):
-        groups[n+1-i]= largest(List1)
+        groups[n+1-i]= largest(List0)
+        #Remove it from the list
+    #If len(List1) == 0:
+        #not_done = False
 print(groups)
 
     #for each group, 1,2,3...n distribute the largest item in the list
